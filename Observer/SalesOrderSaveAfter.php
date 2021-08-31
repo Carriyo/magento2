5da83bc803895c9d3d0aaf685bdcfb5267a31c46
Faisel Azeez <fazeez@gmail.com>
2021-08-31 13:05:04 +0400

    added logging

--- Observer/SalesOrderSaveAfter.php
+++ Observer/SalesOrderSaveAfter.php
@@ -44,6 +44,9 @@ class SalesOrderSaveAfter implements ObserverInterface
      */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
+        $this->logger->info("order status: " . $order->getStatus());
+        $this->logger->info("previous order status: " . $order->getOrigData('status'));
+
         $order = $observer->getEvent()->getOrder();
         if ($order->getStatus() ===  $order->getOrigData('status') ) {
             return $this;
